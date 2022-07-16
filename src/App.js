db1a583ee5528a8308cfdde5cd1217c1079d4c92
Balamurugan <balamurugan@MyGates-MBP.bbrouter>
2022-07-16 10:29:13 +0530

    using unicon icons and created base setup for that

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <UilReact size="140" color="#61DAFB" />
     </div>
   );
 }
